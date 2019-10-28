ed4c675dd187c3b92d50f002e601c1a7a9feb7b8
tempus23 <maciek13U@gmail.com>
2019-10-28 15:48:55 +0100

    map

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Go_tests
 Simple programs in Golang:
 * Simple01: func, struct, input, imports
+* Simple02: strings, arrays, map
--- Simple02/main.go
+++ Simple02/main.go
@@ -7,7 +7,8 @@ import (
 	"github.com/maciek13U/Go_tests/Simple02/strutil"
 )
 
-// LetterInStringCount takes string and letter to count how many times it repeated
+// LetterInStringCount takes string and letter to count how many times
+// it repeated (case sensitive!)
 func LetterInStringCount(testedString string, letter string) int {
 	lettersInString := strings.Count(testedString, letter)
 	return lettersInString
@@ -23,4 +24,25 @@ func main() {
 	fmt.Println(letter, ":", lettersInString)
 
 	fmt.Println(strutil.Reverse("abc123"))
+
+	// var strArray [3]string
+	// strArray[0] = "123"
+	// strArray[2] = "abc"
+	strArray := [3]string{"123", "", "abc"}
+	fmt.Println(strArray)
+
+	// map[type]type
+	var fileExtensions = map[string]string{
+		"Python":     ".py",
+		"C":          ".c",
+		"C++":        ".cpp",
+		"Text":       ".txt",
+		"Executable": ".exe",
+		"Golang":     ".go",
+		"Java":       ".java",
+	}
+
+	if key, search := fileExtensions["Golang"]; search {
+		fmt.Println(key)
+	}
 }
